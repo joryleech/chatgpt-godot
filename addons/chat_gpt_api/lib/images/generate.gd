2df28472e5afa1b1909260250c49c00cdf5f4555
Jory Leech <joryleech@gmail.com>
2023-05-28 17:26:37 -0700

    Added automatic generation of images textures, and sample

--- addons/chat_gpt_api/lib/images/generate.gd
+++ addons/chat_gpt_api/lib/images/generate.gd
@@ -4,8 +4,8 @@ extends ChatGptApi.Request
 @export var size : String = "1024x1024"
 @export var image_count : int = 1
 @export var response_format : String = "url"
-#@export var generate_textures : bool = false TODO Implement
-
+@export var generate_textures : bool = false
+signal on_generate_texture(textures : Array)
 
 func _init():
 	self.api_endpoint = 'images/generations'
@@ -14,12 +14,23 @@ func generate_body() -> String:
 	var body_dictionary : Dictionary = {
 		"prompt": prompt,
 		"n": image_count,
-		"size": size
+		"size": size,
+		"response_format": 'b64_json' if generate_textures else response_format
 	}
 	var json_body : String = JSON.stringify(body_dictionary, ChatGptApi.Config.JSON_DELIMITER)
 	return json_body
-
-func _on_success(response):
-	emit_signal('on_success', response) 
 	
+func base_64_to_texture(image_b64 : String):
+	var i = Image.new()
+	i.load_png_from_buffer(Marshalls.base64_to_raw(image_b64))
+	return ImageTexture.create_from_image(i)
+	
+	
+func _on_success(response):
+	if(generate_textures):
+		var images_to_return : Array = []
+		for image_response in response['data']:
+			images_to_return.append(base_64_to_texture(image_response['b64_json']))
+		emit_signal("on_generate_texture", images_to_return)
+	emit_signal('on_success', response) 
 
--- sample/scenes/Images/ImageSampleScript.gd
+++ sample/scenes/Images/ImageSampleScript.gd
@@ -4,8 +4,9 @@ var text_log : PackedStringArray = []
 @onready var text_input : TextEdit = $HBoxContainer/Input
 @onready var size_input : OptionButton = $OptionsContainer/ImageSize/OptionButton
 @onready var image_count_input : SpinBox = $OptionsContainer/ImageCount/SpinBox
-
-
+@onready var text_output : RichTextLabel = $OutputContainer/UrlOutput
+@onready var image_output_container : HBoxContainer = $OutputContainer/ImageOutput
+@onready var generate_textures_bool : CheckBox = $OptionsContainer/GenerateTextureBool/OptionButton
 func _on_submit_log():
 	var requested_text = text_input.text
 	text_input.text = ""
@@ -13,8 +14,22 @@ func _on_submit_log():
 	_send_message(requested_text)
 	
 func _send_message(message : String):
+	for n in image_output_container.get_children():
+		image_output_container.remove_child(n)
+		n.queue_free()
+	
+
 	var image_generator = ChatGptApi.Images.Generate.new()
 	add_child(image_generator)
+	
+	if(generate_textures_bool.is_pressed()):
+		image_output_container.visible = true
+		text_output.visible = false
+		image_generator.generate_textures = true
+	else:
+		image_output_container.visible = false
+		text_output.visible = true
+		
 	
 	image_generator.prompt = message
 	image_generator.size = size_input.get_item_text(size_input.get_selected_id())
@@ -22,7 +37,14 @@ func _send_message(message : String):
 	image_generator.on_request_completed.connect(self.on_request_completed)
 	image_generator.on_success.connect(self.on_request_success)
 	image_generator.on_failure.connect(self.on_request_error)
+	image_generator.on_generate_texture.connect(self.on_generate_texture)
 	image_generator.request()
+
+func on_generate_texture(textures : Array):
+	for texture in textures:
+		var rect : TextureRect = TextureRect.new()
+		rect.texture = texture
+		image_output_container.add_child(rect)
 	
 func on_request_completed( result,response_code,headers,body, response):
 	print(result)
@@ -31,9 +53,10 @@ func on_request_completed( result,response_code,headers,body, response):
 	print(response_code)
 	print(response)
 
-func on_request_success(response):
+func on_request_success(response : Dictionary):
 	for image_response in response['data']:
-		_add_log("[url]"+image_response['url']+"[/url]", "blue")
+		if(image_response.has('url')):
+			_add_log("[url]"+image_response['url']+"[/url]", "blue")
 	
 func on_request_error(code, response):
 	print("Code"+str(code))
@@ -49,10 +72,9 @@ func _add_log(log : String, color : String = "white"):
 	var converted_log : String = "[color="+color+"]"+log+""
 	if(log.length() > 0):
 		text_log.append(converted_log)
-	$Output.text = "\n".join(text_log)
+	text_output.text = "\n".join(text_log)
 
 
 func _on_output_link_clicked(meta):
-	print(meta)
 	OS.shell_open(meta)
 	pass # Replace with function body.
